ca0114db1e228b949de70b4eb8a2e3c5e2809510
Wei.Wang <1849553578@qq.com>
2025-04-01 19:18:02 +0800

    汇编>从a + b了解汇编语言(X64)

--- 009.汇编语言/001.STU_CODE/003.a_plus_b_v3.c
+++ 009.汇编语言/001.STU_CODE/003.a_plus_b_v3.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-// gcc -S  -fverbose-asm 002.a_plus_b.c 
+// gcc -S  -fverbose-asm 002.a_plus_b.c
 int add(int a, int b);
 
-int main(int argc, char **argv)
-{
-    int result = 0;
+int main(int argc, char **argv) {
+  int result = 0;
 
-    int a = 1, b = 1;
+  int a = 1, b = 1;
 
-    result = add(a, b);
+  result = add(a, b);
 
-    printf("result: %d \n", result);
+  printf("result: %d \n", result);
 
-    return 0;
+  return 0;
 }
 
-int add(int a, int b)
-{
-    int c = a+b;
-    return c;
+int add(int a, int b) {
+  int c = a + b;
+  return c;
 }
